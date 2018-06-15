9d66558d67ea2d50777c8ecf40184c46a557f662
Kit Jonathan Te <kjonathante@gmail.com>
2018-06-15 16:01:20 -0700

    add layout, some events and test jquery test method

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,87 @@
-const characters = [
-  {
-    name: "charac0",
-    healthPoints: 100,
-    attackPower: 100,
-    counterAttackPower: 100
-  },
-  {
-    name: "charac1",
-    healthPoints: 20,
-    attackPower: 5,
-    counterAttackPower: 5
-  },
-  {
-    name: "charac2",
-    healthPoints: 100,
-    attackPower: 100,
-    counterAttackPower: 100
-  },
-  {
-    name: "charac3",
-    healthPoints: 25,
-    attackPower: 6,
-    counterAttackPower: 8
-  },
-];
 
-const playerInput = 3;
+let stage = 0;
 
-const computerInputs = [1,2,0];
 
-const player = Object.create( characters[playerInput] );
-player.computedAttackPower = player.attackPower;
+$('#selection-area').on('click', '.characters',function() {
+  const player = $(this)
+  $('#attack-area').prepend( player );
 
-for (let i = 0; i < computerInputs.length; i++) {
 
-  let computerInput = computerInputs[i];
+  const enemies = $('#selection-area .characters');
+  $('#holding-area').prepend( enemies );
+});
 
-  let enemy = Object.create( characters[computerInput] );
+$('#holding-area').on('click', '.characters',function() {
+  $('#defense-area').prepend( $(this) );
+});
 
-  console.log(`enemy hp: ${enemy.healthPoints}, counter attack power: ${enemy.counterAttackPower}`);
-  console.log(`player hp: ${player.healthPoints}, computed attack power: ${player.computedAttackPower}`);
+// const characters = [
+//   {
+//     name: "charac0",
+//     healthPoints: 100,
+//     attackPower: 100,
+//     counterAttackPower: 100
+//   },
+//   {
+//     name: "charac1",
+//     healthPoints: 20,
+//     attackPower: 5,
+//     counterAttackPower: 5
+//   },
+//   {
+//     name: "charac2",
+//     healthPoints: 100,
+//     attackPower: 100,
+//     counterAttackPower: 100
+//   },
+//   {
+//     name: "charac3",
+//     healthPoints: 25,
+//     attackPower: 6,
+//     counterAttackPower: 8
+//   },
+// ];
 
-  while ( player.healthPoints > 0 && enemy.healthPoints > 0 ) {
-    //attack
-    enemy.healthPoints -= player.computedAttackPower;
-    //increment
-    player.computedAttackPower += player.attackPower;
+// const playerInput = 3;
 
-    //counter attack
-    if (enemy.healthPoints > 0) {
-      player.healthPoints -= enemy.counterAttackPower;
-    } else {
-      console.log("enemy is dead and can't hit no more.")
-    }    
-    console.log(`enemy hp: ${enemy.healthPoints}`)
-    console.log(`player hp: ${player.healthPoints}`)
-    console.log(`player computed attach power: ${player.computedAttackPower}`);
-  }
+// const computerInputs = [1,2,0];
 
-  if (player.healthPoints < 1) {
-    console.log("player is dead");
-    break;
-  }
-}
-// $.each( characters, (index, char) => {
-//   $.each( char, (key, value) => {
-//     console.log( `${key}: ${value}` );
-//   });
-// });
+// const player = Object.create( characters[playerInput] );
+// player.computedAttackPower = player.attackPower;
+
+// for (let i = 0; i < computerInputs.length; i++) {
+
+//   let computerInput = computerInputs[i];
+
+//   let enemy = Object.create( characters[computerInput] );
+
+//   console.log(`enemy hp: ${enemy.healthPoints}, counter attack power: ${enemy.counterAttackPower}`);
+//   console.log(`player hp: ${player.healthPoints}, computed attack power: ${player.computedAttackPower}`);
+
+//   while ( player.healthPoints > 0 && enemy.healthPoints > 0 ) {
+//     //attack
+//     enemy.healthPoints -= player.computedAttackPower;
+//     //increment
+//     player.computedAttackPower += player.attackPower;
+
+//     //counter attack
+//     if (enemy.healthPoints > 0) {
+//       player.healthPoints -= enemy.counterAttackPower;
+//     } else {
+//       console.log("enemy is dead and can't hit no more.")
+//     }    
+//     console.log(`enemy hp: ${enemy.healthPoints}`)
+//     console.log(`player hp: ${player.healthPoints}`)
+//     console.log(`player computed attach power: ${player.computedAttackPower}`);
+//   }
+
+//   if (player.healthPoints < 1) {
+//     console.log("player is dead");
+//     break;
+//   }
+// }
+// // $.each( characters, (index, char) => {
+// //   $.each( char, (key, value) => {
+// //     console.log( `${key}: ${value}` );
+// //   });
+// // });
